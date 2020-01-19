dc79e5f38d6aa1db9282d2737e94b396c23a3cad
nabila <nabilatajrin@gmail.com>
2020-01-19 16:08:48 +0600

    update

--- download-images-from-url-list.py
+++ download-images-from-url-list.py
@@ -2,14 +2,15 @@ import pandas as pd
 import requests
 
 df = pd.read_csv('bkash_banner3.csv')
-
 image_url = df['Url'].head()
 
 for img in image_url:
      file_name = img.split('/')[-1]
+     file_path = r'/media/user/A/github/python/image-downloader-from-csv/images/%s'%file_name
      print("Downloading file:%s"%file_name)
      r = requests.get(img, stream=True)
-     
-     with open(file_name, 'wb') as f:
+
+    #download images
+     with open(file_path, 'wb') as f:
             for chunk in r:
                   f.write(chunk)
