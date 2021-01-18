649a2da961ba4e1935bdb68d8ee822aae7c1333a
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2021-01-19 01:29:05 +0600

    Update download-images-from-url-list.py

--- download-images-from-url-list.py
+++ download-images-from-url-list.py
@@ -9,7 +9,6 @@ for img in image_url:
      file_path = r'/media/user/A/github/python/image-downloader-from-csv/images/%s'%file_name
      print("Downloading file:%s"%file_name)
      r = requests.get(img, stream=True)
-
     #download images
      with open(file_path, 'wb') as f:
             for chunk in r:
